08413dfa9f757b3512b1141696acdd9c6eb3216b
Vaerilius <tjohannest@gmail.com>
2019-03-26 09:54:31 +0200

    localhost

--- FirstExpessApp/app.js
+++ FirstExpessApp/app.js
@@ -1,11 +1,21 @@
 var express = require('express');
 
 var app = express();
-
+const port = 3000;
 app.get('/', function (request, response) {
-   request.send('Hi there');
+   response.send('Hi there');
+});
+app.get('/test', function (request, response) {
+   response.send('testing');
 });
 
-app.listen(process.env.PORT, process.env.IP, function() {
-   console.log('server has started');
+app.get('/winter', function (request, response) {
+   response.send('No is not, but GOG start soon');
 });
+
+
+app.get('*', function (req, res) {
+   res.send('page not found');
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
