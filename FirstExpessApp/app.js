027acd51df3c6f3dafad04c0769d1ad574b61025
Vaerilius <tjohannest@gmail.com>
2019-03-26 10:19:27 +0200

    dynamic

--- FirstExpessApp/app.js
+++ FirstExpessApp/app.js
@@ -13,6 +13,15 @@ app.get('/winter', function (request, response) {
    response.send('No is not, but GOG start soon');
 });
 
+app.get('/r/:subName', function (req, res) {
+   var subNames = req.params.subName;
+   console.log(req.params);
+   res.send(`welcometo the  ${subNames.toUpperCase()} subnames`);
+});
+app.get('/r/:subName/comments/:id/:title/', function (req, res) {
+   console.log(req.params);
+   res.send('Welcome to the comments page');
+});
 
 app.get('*', function (req, res) {
    res.send('page not found');
