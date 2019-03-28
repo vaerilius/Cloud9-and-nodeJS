ae7a1615e4d518d5e648371a6487b94d9fd25cc8
Vaerilius <tjohannest@gmail.com>
2019-03-28 09:55:48 +0200

    style and pachs

--- moreExpress/EJSDemo/app.js
+++ moreExpress/EJSDemo/app.js
@@ -1,13 +1,29 @@
+//add express to app
 var express = require('express');
 var app = express();
 
+//import public dorectory to app
+app.use(express.static('public'));
+
+//set app to use .ejs by defauld
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
-   res.render('home.ejs');
+   res.render('home');
 });
 
 app.get('/fallinglovewith/:thing', function (req, res) {
-var thing = req.params.thing;
-res.render('love.ejs', {thingVar: thing});
+   var thing = req.params.thing;
+   res.render('love', { thingVar: thing });
+});
+
+app.get('/posts', function (req, res) {
+var posts =  [
+   {title: 'Post 1', author: 'Timo'},
+   {title: 'Testing', author: 'Pekka'},
+   {title: 'Hello there', author: 'AI'}
+];
+   res.render('posts', {posts: posts});
 });
 
 app.listen(3000, () => console.log('localhost:3000'));
